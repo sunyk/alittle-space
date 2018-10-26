7923357edb2c2be4ba7a5b5eaab26e886dca82ae
sunyang <sunyang@tuhu.cn>
2018-10-26 09:52:57 +0800

    10.26代码打卡

---
+++ usedemo/src/main/java/com/sunyk/ontheroad/StackImplQueue.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2018 tuhu.cn All right reserved. This software is the
+ * confidential and proprietary information of tuhu.cn ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with Tuhu.cn
+ */
+package com.sunyk.ontheroad;
+
+import java.util.Stack;
+
+/**
+ * @author sunyang
+ * @date 2018/10/25 10:18
+ */
+public class StackImplQueue {
+
+    /**
+     * 定义两个栈来实现队列
+     * 栈A 负责插入新元素
+     * 栈B 负责移除老元素
+     */
+    private Stack<Integer> stackA = new Stack<>();
+    private Stack<Integer> stackB = new Stack<>();
+
+    /**
+     * 入队操作
+     * @Param element
+     */
+    public void enQueue(int element){
+        stackA.push(element);
+    }
+
+    /**
+     *
+     * 出队操作
+     */
+    public Integer deQueue(){
+        if (stackB.isEmpty()){
+            if (stackA.isEmpty()){
+                return null;
+            }
+            fetchFormStackA();
+        }
+
+        return stackB.pop();
+    }
+
+    /**
+     * 从stackA栈中拿到出栈元素压入栈B
+     */
+    private void fetchFormStackA() {
+        while (!stackA.isEmpty()){
+            stackB.push(stackA.pop());
+        }
+    }
+
+    public static void main(String[] args) {
+        StackImplQueue stackQueue = new StackImplQueue();
+        stackQueue.enQueue(1);
+        stackQueue.enQueue(2);
+        stackQueue.enQueue(3);
+
+        System.out.println(stackQueue.deQueue());
+        System.out.println(stackQueue.deQueue());
+        System.out.println(stackQueue.deQueue());
+
+        stackQueue.enQueue(4);
+        System.out.println(stackQueue.deQueue());
+
+
+    }
+}
+
